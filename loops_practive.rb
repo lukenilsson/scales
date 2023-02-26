79f9683bcf6b58fe646091626dba3e0246cc685c
“lukenilsson” <luke.nilsson@gmail.com>
2023-02-26 16:10:49 -0500

    loop practice

---
+++ loops_practive.rb
@@ -0,0 +1,82 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+# number = 1
+# 10.times do
+#   puts number
+#   number += 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# word = "hello"
+# 5.times do
+#   puts word
+# end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+# while true
+#   puts "Please enter a word!"
+#   word = gets.chomp
+#   if word == "stop"
+#     puts "Okay bye bye now"
+#     break
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+# number = 0
+
+# while number <= 100
+#   puts number
+#   number += 5
+# end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+# index = 0
+# while index < 10
+#   puts "9000"
+#   index += 1
+# end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+# number = 0
+# while number <= 10
+#   puts "Please enter in a number!"
+#   number = gets.chomp.to_i
+#   if number > 10
+#     puts "Game Over"
+#     break
+#   end
+# end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+# number = 50
+
+# while number <= 70
+#   puts number
+#   number += 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# number = 0
+# while number < 144
+#   puts "Around the world"
+#   number += 1
+# end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   puts "Please enter some words!"
+#   word = gets.chomp
+#   if word.length > 5
+#     puts "Game Over"
+#     break
+#   end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+# number = 2
+# while number <= 40
+#   puts number
+#   number += 2
+# end
